5af71eb09190acf7ed209178644a5c94300b82b8
Amanda <AC350144@gmail.com>
2025-05-04 16:06:30 -0700

    moved server.js to root directory.

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+
+require('dotenv').config({ path: __dirname + '/.env' });
+
+// Middleware setup
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'frontend')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// API Routes
+app.use('/api/auth', require('./backend/routes/authRoutes'));
+app.use('/api/listings', require('./backend/routes/listingsRoutes'));
+app.use('/api/bookings', require('./backend/routes/bookingsRoutes'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'home.html'));
+});
+
+// MongoDB connection and server setup
+
+console.log('MONGO_URI is:', process.env.MONGO_URI);
+const PORT = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch(err => console.error(err));
+  
+module.exports = app;
