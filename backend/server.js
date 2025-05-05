49f38acd87c18761c8311dda353054d020653610
Amanda <AC350144@gmail.com>
2025-05-04 17:25:17 -0700

    revert

--- backend/server.js
+++ backend/server.js
@@ -15,12 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, 'frontend', 'build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
-});
-
+app.use(express.static(path.join(__dirname, 'frontend')));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // API Routes
