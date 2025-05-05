e68ea3c00aa10b6da8d2ee026bfbd412fc752460
Amanda <AC350144@gmail.com>
2025-05-04 17:18:55 -0700

    fixing frontend deployment issue hopefully

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, 'frontend')));
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+});
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // API Routes
